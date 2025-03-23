78de722c25f5a130fd4b2f72329d777727a21b3b
KyawZayYa <dev.kyawzayya.kzy@gmail.com>
2025-03-23 21:57:27 +0800

    Revert "JWT Auth first commit"
    This reverts commit 228337d7095a51ff99361d1da837d5a6c9a5cdb9.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,21 +4,6 @@ import { LayoutComponent } from './pages/layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CustomerComponent } from './pages/customer/customer.component';
 import { ProductsComponent } from './products/products.component';
-import { inject } from '@angular/core';
-import { Router } from 'express';
-
-
-const authGuard = () => {
-    const token = localStorage.getItem('accessToken');
-    const router = inject(Router);
-
-    if(!token) {
-        router.navigate(['/login']);
-        return false;
-    }
-
-    return true;
-};
 
 export const appRoutes: Routes = [
     {
@@ -33,7 +18,6 @@ export const appRoutes: Routes = [
     {
         path: '',
         component: LayoutComponent,
-        canActivate: [authGuard],
         children: [
             {
                 path: 'dashboard',
@@ -49,5 +33,4 @@ export const appRoutes: Routes = [
             },
         ]
     }
-    
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,46 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, HttpClientModule], // Add HttpClientModule here
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  userName = new FormControl('');
-  password = new FormControl('');
+
+  userName: FormControl = new FormControl();
+
+  password: FormControl = new FormControl();
 
   router = inject(Router);
-  http = inject(HttpClient);
 
   onLogin() {
-    console.log('Login button clicked');
-
-    const username = this.userName.value;
-    const password = this.password.value;
-
-    if (!username || !password) {
-      alert('Please enter both username and password');
-      return;
+    debugger;
+    if(this.userName.value === 'admin' && this.password.value === '2233')
+    {
+      this.router.navigate(['/dashboard']);
     }
-
-    this.http.post<any>('https://localhost:7155/api/Onboarding/Login', {
-      username,
-      password
-    }).subscribe({
-      next: (res) => {
-        localStorage.setItem('accessToken', res.accessToken);
-        localStorage.setItem('refreshToken', res.refreshToken);
-        this.router.navigate(['/products']);
-      },
-      error: (err) => {
-        alert('Invalid credentials');
-      }
-    });
+    else
+    {
+      alert('Invalid Credentials');
+    }
   }
 }
