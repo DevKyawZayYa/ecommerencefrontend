701fa80469fc0a3a5f578b2f9bf79d4c04789f1d
KyawZayYa <dev.kyawzayya.kzy@gmail.com>
2025-04-29 21:39:49 +0800

    change env to prod api-url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://13.215.183.114:5000/api',
-    swaggerUrl: 'https://13.215.183.114:5000/swagger'
+    apiUrl: 'https://swagger-nshoppe.com/api',
+    swaggerUrl: 'https://swagger-nshoppe.com/swagger'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,9 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7155/api',
-    swaggerUrl: 'https://localhost:7155/swagger'
+    // apiUrl: 'https://localhost:7155/api',
+    // swaggerUrl: 'https://localhost:7155/swagger'
+
+    apiUrl: 'https://swagger-nshoppe.com/api',
+    swaggerUrl: 'https://swagger-nshoppe.com/swagger'
 };
   
